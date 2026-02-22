cafca06d1e4e0c25d48e4ddc860f5b8a88ce3a0d
Henry <102833022+SeizeOP@users.noreply.github.com>
2026-02-21 21:43:58 -0500

    Update Emacs Config
    Added Markdown mode, plus other cleanup.

--- emacs/init.el
+++ emacs/init.el
@@ -15,7 +15,8 @@
      "\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)"
      "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(package-selected-packages
-   '(markdown-preview-eww markdown-ts-mode page-break-lines web-server))
+   '(markdown-preview-eww markdown-preview-mode markdown-ts-mode
+			  page-break-lines web-server))
  '(package-vc-selected-packages
    '((doom-dashboard :url
 		     "https://github.com/emacs-dashboard/doom-dashboard.git")))
